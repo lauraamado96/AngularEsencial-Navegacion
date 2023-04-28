8bc056577ccaf60eac212c0e135c74c49bedf47a
lauraamado96 <laura.amado.96@hotmail.com>
2023-04-28 23:15:32 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { Error404Component } from './error404/error404.component';
+import { ListaDeLibrosComponent } from './lista-de-libros/lista-de-libros.component';
+
+const routes: Routes = [
+  { path: 'lista-libros', component: ListaDeLibrosComponent },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: 'detalles/:libroId', component: DetallesComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'lista-libros' },
+  { path: '404', component: Error404Component },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { Error404Component } from './error404/error404.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListaDeLibrosComponent } from './lista-de-libros/lista-de-libros.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    DetallesComponent,
+    EncabezadoComponent,
+    Error404Component,
+    InicioComponent,
+    ListaDeLibrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
